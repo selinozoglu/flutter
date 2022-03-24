9310e1735cbe8a09f0e67688d3b4b344ee2b6831
selin <selin-ozoglu@outlook.com>
2022-03-24 17:51:52 +0300

    delete button works

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project1/models/employee.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,40 +8,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Employees',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.indigo,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Employees'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,68 +29,117 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+  Employee selectedEmployee = Employee.withId(0,'', '', 0, '');
+  List<Employee> employeeList = [
+    Employee.withId(1, 'Selin', 'Özoğlu', 1, 'Frontend Developer'),
+    Employee.withId(2, 'Ali', 'Duru', 13, 'Fullstack Developer'),
+    Employee.withId(3, 'Ece', 'Yılmaz', 18, 'Backend Developer'),
+    Employee.withId(4, 'Gökhan', 'Bilen', 5, 'Backend Developer')
+  ];
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(widget.title + ' ' + setMeetTitle()),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+              child: ListView.builder(
+                  itemCount: employeeList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                        leading: CircleAvatar(
+                          backgroundColor: Colors.lightGreenAccent,
+                        ),
+                        title: Text(employeeList[index].name +
+                            ' ' +
+                            employeeList[index].surname),
+                        subtitle: Text('Level: ' +
+                            employeeList[index].level.toString() +
+                            ' ' +
+                            employeeList[index].getInfo),
+                        trailing: buildStatusIcon(employeeList[index].level),
+                        onTap: () {
+                          setState(() {
+                            selectedEmployee = employeeList[index];
+                          });
+                        });
+                  })),
+
+          Row(
+            children: <Widget>[
+              Flexible(
+                fit: FlexFit.tight,
+                flex: 1,
+                child: RaisedButton(
+                  color: Colors.lime,
+                  child: Row(
+                    children: [Text('add'), Icon(Icons.add_circle_outline)],
+                  ),
+                  onPressed: () {
+                    print('add');
+                  },
+                ),
+              ),
+              Flexible(
+                fit: FlexFit.tight,
+                flex: 1,
+                child: RaisedButton(
+                  color: Colors.amberAccent,
+                  child: Row(
+                    children: [Text('update'), Icon(Icons.update_outlined)],
+                  ),
+                  onPressed: () {
+                    print('update');
+                  },
+                ),
+              ),
+              Flexible(
+                fit: FlexFit.tight,
+                flex: 1,
+                child: RaisedButton(
+                  color: Colors.deepOrangeAccent,
+                  child: Row(
+                    children: [Text('delete'), Icon(Icons.highlight_remove)],
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      employeeList.remove(selectedEmployee);
+                    });
+                    var alert = AlertDialog(
+                        title: Text('Result'), content: Text('Deleted'));
+                    showDialog(context: context, builder: (context) => alert);
+
+                  },
+                ),
+              ),
+            ],
+          ),
+        ],
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget buildStatusIcon(int level) {
+    if (level > 10) {
+      return Icon(Icons.check_circle_outline);
+    } else {
+      return Icon(Icons.access_time_outlined);
+    }
+  }
+}
+
+String setMeetTitle() {
+  DateTime now = new DateTime.now();
+  int hour = now.hour;
+  if (hour < 12) {
+    return 'Good Morning';
+  } else if (hour < 18) {
+    return 'Good Day';
+  } else {
+    return 'Good Evening';
   }
 }
